5402284dbafb6c18dbd71c6eb0b9964e0223eb32
fluecubic <jonas.lorenz1234567890@gmail.com>
2026-02-18 22:28:16 +0100

    GIG

--- CardClash/Cards/script.js
+++ CardClash/Cards/script.js
@@ -1,6 +1,6 @@
 import {user} from "../../login/login.js"
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";  
-import { getDoc, addDoc, doc, getFirestore, getDocs, getDocFromCache, collection, updateDoc, Timestamp, onSnapshot, query, orderBy, serverTimestamp, setDoc, limit } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";//init befehle
+//import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";  
+//import { getDoc, addDoc, doc, getFirestore, getDocs, getDocFromCache, collection, updateDoc, Timestamp, onSnapshot, query, orderBy, serverTimestamp, setDoc, limit } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";//init befehle
 
 const firebaseConfig = {
     apiKey: "AIzaSyBL3-DyIr8JEiRbPfGcvfzQ0HLc6auHrvE",
@@ -12,7 +12,7 @@ const firebaseConfig = {
     measurementId: "G-1QFPXXQSEF"
   };
 
-const db = getFirestore(initializeApp(firebaseConfig));
+//const db = getFirestore(initializeApp(firebaseConfig));
 
 const response = await fetch("./cards.json");
 const CardInfo = await response.json();
@@ -31,12 +31,12 @@ console.log(CardInfo)
     if (user.uid || isBot) {
        console.log(user) 
     } else {
-      window.location = "../../login/login.html"
+      //window.location = "../../login/login.html"
     }
 
 
     async function getUserInfo(uid) {
-        const q = query(collection(db, "users"));
+        //const q = query(collection(db, "users"));
         let userInfo = new Object();
     
          const Snapshot = await getDocs(q);
@@ -61,13 +61,19 @@ console.log(CardInfo)
         return userInfo;
     }
     
-     let Me = await getUserInfo(user.uid)
+     let Me = {
+      Vorname: "Jonas",
+      Nachname: "Lorenz",
+      Klasse: "8/6",
+      Cards: ["0x5", "1x3"]
+     }
+     //await getUserInfo(user.uid)
 
 function loadCards() {
     for (let i = 0; i < Me.Cards.length; i++) {
         let Number = Me.Cards[i].split("x")[0]
         let Amount = Me.Cards[i].split("x")[1]
-        document.getElementById("MyCards").innerHTML += "<div  class='Card " + "Card-"+ Number + "'><img class='CardImg " + "Card-"+ Number + "' src='"+ CardInfo[i].img +"'><p class='CardNameSmall " + "Card-"+ Number + "'>" + CardInfo[i].Name + "</p> </div>"
+        document.getElementById("MyCards").innerHTML += "<div  class='Card " + "Card-"+ Number + "'><img class='CardImg " + "Card-"+ Number + "' src='"+ CardInfo.Cards[i].img +"'><p class='CardNameSmall " + "Card-"+ Number + "'>" + CardInfo.Cards[i].Name + "</p> </div>"
         document.getElementsByClassName("Card")[i].setAttribute('data-before', Amount)
         
      }
@@ -78,7 +84,7 @@ loadCards()
 document.addEventListener("click", function (event) {
   if (event.target.classList[1] && event.target.classList[1].includes("Card")) {
       console.log()
-     document.getElementsByTagName("body")[0].innerHTML += "<div class='blurry'><div id='viewCard'><img id='CardBig' src='" + CardInfo[event.target.classList[1].split("-")[1]].img +"'><p id='esc'>x</p><button id='SellButton'>Angebot erstellen</button></div></div>";
+     document.getElementsByTagName("body")[0].innerHTML += "<div class='blurry'><div id='viewCard'><img id='CardBig' src='" + CardInfo.Cards[event.target.classList[1].split("-")[1]].img +"'><p id='esc'>x</p><button id='SellButton'>Angebot erstellen</button></div></div>";
 
      document.getElementById("esc").addEventListener("click", function () {
      document.getElementsByClassName("blurry")[0].remove()
@@ -87,3 +93,60 @@ document.addEventListener("click", function (event) {
   }
 })
 
+function loadPacks() {
+  for (let i = 0; i < CardInfo.Packs.length; i++) {
+        document.getElementById("CardPacks").innerHTML += "<div  class='Pack " + "Pack-"+ i + "'><img class='CardImg " + "PackImg-"+ i + "' src='"+ CardInfo.Packs[i].img +"'> <div class='flex'> <p class='CardNameSmall " + "Card-"+ i + "'>" + CardInfo.Packs[i].Name + "</p> <p class='Arrow " + "Arrow-" + i + "'> < </p> </div></div>"
+        document.getElementsByClassName("Pack")[i].setAttribute('data-prize', String(CardInfo.Packs[i].Price) + "₦")
+        console.log(String(CardInfo.Packs[i].Price))
+        
+     }
+}
+
+let PacksInfoOpen = [false, false, false, false, false]
+
+document.addEventListener("click", function (event) {
+  if (event.target.classList[0] == "Arrow") {
+    let n = Number(event.target.classList[1].split("-")[1])
+    if (PacksInfoOpen[n]) {
+      document.getElementsByClassName("PackInfo-" + String(n))[0].remove()
+      PacksInfoOpen[n] = false
+      document.getElementsByClassName("Arrow-" + String(n))[0].style.transform = "rotate(0deg)"
+    } else {
+      PacksInfoOpen[n] = true
+      document.getElementsByClassName("Pack-" + String(n))[0].innerHTML += "<p class='PackInfo PackInfo-" + n + "'>Enthält "+ String(CardInfo.Packs[n].Cards) +" Karten <br>Gewönhlich: "+ String(CardInfo.Packs[n].Chances[0] * 100) +"%<br>Selten: "+ String(CardInfo.Packs[n].Chances[1] * 100) +" %<br>Episch: "+ String(CardInfo.Packs[n].Chances[2] * 100) +" %<br>Legendär: "+ String(CardInfo.Packs[n].Chances[4] * 100) +" %<br>Unterstützerkarten: "+ String(CardInfo.Packs[n].Chances[3] * 100) +" %<p>"
+      document.getElementsByClassName("Arrow-" + String(n))[0].style.transform = "rotate(180deg)"
+    }
+  }
+})
+
+loadPacks()
+
+let rarity;
+let tries = 0;
+let clickedNumber = 0;
+
+function gamble() {
+  if (tries < CardInfo.Packs[clickedNumber].Cards) {
+    let RNG = Math.random();
+    if (RNG < CardInfo.Packs[clickedNumber].Chances[0]) //Baustelle
+      
+      
+      
+      console.log(rarity)
+      tries++
+    } else {
+      document.getElementsByClassName("blurry")[0].removeEventListener("click", gamble)
+      document.getElementsByClassName("blurry")[0].remove() 
+    }
+}
+
+function OpenPack() {
+  document.getElementsByTagName("body")[0].innerHTML +=  "<div class='blurry'><div>"
+  
+
+    document.getElementsByClassName("blurry")[0].addEventListener("click", gamble)
+    
+}
+
+
+OpenPack()
